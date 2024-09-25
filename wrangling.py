0496384d38312c614e361b1272b1e615b2b98021
jipijipi <jp.leflot@gmail.com>
2024-09-25 23:44:59 +0200

    added crossref api call

--- wrangling.py
+++ wrangling.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import json
+import time
 
 
 def flatten(dictionnary, prefix=''):
@@ -26,28 +27,6 @@ def flatten(dictionnary, prefix=''):
     return flattened
 
 
-def get_all_laureates(url):
-    offset = 0
-    limit = 25
-    max_offset = 50
-    all_laureates = pd.DataFrame()
-    while offset < max_offset:
-        final_url = f"{url}?offset={offset}&limit={limit}"
-        response = requests.get(final_url, timeout=10)
-        data = response.json()
-        max_offset = data['meta']['count']
-        flattened = flatten(data['laureates'])
-        all_laureates = pd.concat(
-            [all_laureates, flattened], ignore_index=True)
-        offset += limit
-
-    all_laureates['id'] = all_laureates['id'].astype(int)
-    all_laureates = all_laureates.reset_index(drop=True)
-    all_laureates = all_laureates.sort_values('id')
-
-    return all_laureates
-
-
 def get_selected_columns(column_dict: dict) -> list:
 
     selected_columns = [infos['original_name']
@@ -57,7 +36,7 @@ def get_selected_columns(column_dict: dict) -> list:
 
 def get_new_names(column_dict: dict) -> dict:
 
-    new_names = {infos['original_name']                 : new for new, infos in column_dict.items()}
+    new_names = {infos['original_name']: new for new, infos in column_dict.items()}
     return new_names
 
 
@@ -103,3 +82,80 @@ def get_json(json_filename):
     except json.JSONDecodeError:
         print(f"Error: The file {json_filename}.json is not a valid JSON.")
         raise
+
+
+def get_all_laureates(url):
+
+    offset = 0
+    limit = 25
+    max_offset = 50
+    all_laureates = pd.DataFrame()
+    while offset < max_offset:
+        final_url = f"{url}?offset={offset}&limit={limit}"
+        response = requests.get(final_url, timeout=10)
+        data = response.json()
+        max_offset = data['meta']['count']
+        flattened = flatten(data['laureates'])
+        all_laureates = pd.concat(
+            [all_laureates, flattened], ignore_index=True)
+        offset += limit
+
+    all_laureates['id'] = all_laureates['id'].astype(int)
+    all_laureates = all_laureates.reset_index(drop=True)
+    all_laureates = all_laureates.sort_values('id')
+
+    return all_laureates
+
+
+def get_given_names(year: int, field: str, count: int) -> dict:
+    url = 'https://api.crossref.org/works'
+    headers = {
+        'User-Agent': 'RandomPhysicsPaperFetcher/1.0 (mailto:jipijipijipi@gmail.com)'
+    }
+    params = {
+        'query': field,
+        'filter': f'type:journal-article,from-pub-date:{year}-01-01,until-pub-date:{year}-12-31',
+        'sample': '50'
+    }
+
+    response = requests.get(url, params=params, headers=headers, timeout=10)
+    response.raise_for_status()
+
+    # Handle rate limiting based on headers
+    rate_limit_remaining = int(
+        response.headers.get('X-Rate-Limit-Remaining', 1))
+    rate_limit_reset = int(response.headers.get('X-Rate-Limit-Reset', 1))
+
+    if rate_limit_remaining == 0:
+        time.sleep(rate_limit_reset)
+        return
+
+    data = response.json()
+    items = data['message']['items']
+
+    given_names = []
+    for item in items:
+        authors = item.get('author', [])
+        if authors:
+            for author in authors:
+                given = author.get('given', '')
+
+                if len(given) > 1 and not any(char in ['.', ' ', '&'] for char in given):
+                    given_names.append(given)
+
+                if len(given_names) >= count:
+                    return given_names
+    authors = {'year': year, 'field': field, 'authors': given_names}
+    return authors
+
+
+def get_all_names_df(dictionary, starting_year) -> pd.DataFrame:
+    all_names = []
+
+    for year in range(starting_year, time.localtime().tm_year - 1):
+        for field in dictionary['field']['categories']:
+            names = get_given_names(year, field, 10)
+            all_names.append(names)
+
+    all_names_df = pd.DataFrame(all_names)
+    return all_names_df
